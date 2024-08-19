87d8b5e696dd0ce40d7dfcc4ec3bab8dea6bc799
Maxime-T <maxime.tscharner@gmail.com>
2024-08-19 13:03:41 +0200

    JE SUIS LEONARD DE VINCI DU CODE EZZZ MERGE

--- Cards/card_ui.gd
+++ Cards/card_ui.gd
@@ -1,6 +1,35 @@
 extends Control
 class_name Card
 
+@export var CardName : String = ""
+
+@export_category("Card stats")
+# -1 pour enlever
+@export var GoldCost : float = 0
+@export var WaterCost : float = -1
+@export var PollutionProd : float = -1
+@export var SunProd : float = -1
+@export var GrowTime : float = -1
+@export var Income : float = -1
+
+@export_group("Labels")
+@export var GoldCostLabel : Label
+@export var WaterCostLabel : Label
+@export var PollutionLabel : Label
+@export var TimeLabel : Label
+@export var SunProdLabel : Label
+@export var IncomeLabel : Label
+
+@export var CardNameLabel : Label
+
+@export_group("HBoxs")
+@export var GoldCostBox : HBoxContainer
+@export var WaterCostBox : HBoxContainer
+@export var PollutionBox : HBoxContainer
+@export var TimeBox : HBoxContainer
+@export var SunProdBox : HBoxContainer
+@export var IncomeBox : HBoxContainer
+
 var scale_size = 1.3
 
 var confirmed : bool:
@@ -8,7 +37,24 @@ var confirmed : bool:
 		if not val:
 			scale = Vector2(1,1)
 		confirmed = val
-		
+
+
+func _ready():
+	CardNameLabel.text = CardName
+	if GoldCost == -1:
+		GoldCostBox.queue_free()
+	if WaterCost == -1:
+		WaterCostBox.queue_free()
+	if PollutionProd == -1:
+		PollutionBox.queue_free()
+	if GrowTime == -1:
+		TimeBox.queue_free()
+	if SunProd == -1:
+		SunProdBox.queue_free()
+	if Income == -1:
+		IncomeBox.queue_free()
+	
+
 func _input(event):
 	if event.is_action_pressed("right click"):
 		release_focus()
