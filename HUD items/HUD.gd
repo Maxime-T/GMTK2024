525e801c45e53e53e69f3e49701b2dc1ae5f8a7d
Adrian Morel <adriandi@hotmail.ch>
2024-08-20 03:28:57 +0200

    Jauge + carte + corn + truc + manchin + tkt + ca boufffe un max de codeeee

--- HUD items/HUD.gd
+++ HUD items/HUD.gd
@@ -2,7 +2,16 @@ extends CanvasLayer
 
 @export var GoldLabel : Label 
 @export var SunLabel : Label 
-@export var PollutionLabel : Label 
+@export var PollutionLabel : Label
+
+@export var HaricotSprite : Sprite2D
+var base_y : int = 760
+var max_y : int = 294
+var pixel_up : int = 0
+
+var y_tier1 : int = 670
+var y_tier2 : int = 547
+var y_tier3 : int = 415
 
 func _ready():
 	Global.G_S_P_changed.connect(update_topHUD)
@@ -12,3 +21,7 @@ func update_topHUD():
 	GoldLabel.text = str(Global.gold)
 	SunLabel.text = str(Global.sun)
 	PollutionLabel.text = str(Global.pollution)
+	
+	pixel_up = int(Global.sun) / 10
+	if base_y - pixel_up >= max_y:
+		HaricotSprite.position.y = base_y - pixel_up
