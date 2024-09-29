e7dc01b155878123cde69d82470e0e1ed201a20f
Maxime Tsch <maxime.tscharner@epfl.ch>
2024-09-29 19:12:21 +0200

    ca marche a pet près

--- Plants/Stats/Stats.gd
+++ Plants/Stats/Stats.gd
@@ -2,20 +2,23 @@
 extends Resource
 class_name Stats
 
-@export_group("Costs")
-@export var goldCost : ModifiableValue
-@export var waterNeeded : ModifiableValue
-@export var pollutionGeneration : ModifiableValue
+var values : Array[Dictionary]
 
-@export_group("Production")
-@export var income : ModifiableValue
-@export var score : ModifiableValue
-
-@export_group("modifiers")
-@export var water : ModifiableValue
-@export var growSpeed : ModifiableValue
+@export_group("ModifiableValue")
+#costs
+var goldCost : ModifiableValue
+var waterNeeded : ModifiableValue
+var pollutionGeneration : ModifiableValue
+#Production
+var income : ModifiableValue
+var score : ModifiableValue
+#modifiers
+var water : ModifiableValue
+var growSpeed : ModifiableValue
 
 func _init() -> void:
+	print(get_property_list())
+	resource_local_to_scene = true
 	goldCost = ModifiableValue.new()
 	waterNeeded = ModifiableValue.new()
 	pollutionGeneration = ModifiableValue.new()
@@ -25,3 +28,26 @@ func _init() -> void:
 
 	water = ModifiableValue.new()
 	growSpeed = ModifiableValue.new(1)
+	
+	values = get_property_list().filter(func(e): return e.class_name == &"ModifiableValue")
+
+func _get_property_list() -> Array:
+	var proprieties : Array[Dictionary]
+	
+	for e in values:
+		proprieties.append({name = e.name+":baseValue", type = TYPE_FLOAT})
+	
+	return proprieties
+
+func _get(property: StringName):
+	if property.ends_with(":baseValue"):
+		var l = ":baseValue".length()
+		return get(property.erase(property.length()-l,l)).baseValue
+	return null
+
+func _set(property: StringName, value) -> bool:
+	if property.ends_with(":baseValue"):
+		var l = ":baseValue".length()
+		get(property.erase(property.length()-l,l)).baseValue = value
+		return true
+	return false
